8ffd1498d451c116fe60c2388e4842a20c0c8503
MariusAD <mariusadicu@gmail.com>
2022-02-15 13:56:51 -0600

    TestNG done

---
+++ src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/TestNG_Intro.java
@@ -0,0 +1,44 @@
+package com.cydeo.tests.day5_testNG_intro_dropdowns;
+
+import org.testng.annotations.*;
+
+public class TestNG_Intro {
+
+    @BeforeClass
+    public void setupMethod(){
+        System.out.println("-----> BeforeClass is running!");
+
+    }
+
+    @AfterClass
+    public void tearDownClass(){
+        System.out.println("-----> AfterClass is running!");
+
+    }
+
+    @BeforeMethod
+    public void setUpMethod(){
+        System.out.println("---> BeforeMethod is running!");
+    }
+
+    @AfterMethod
+    public void tearDownMethod(){
+        System.out.println("---> AfterMethod is running!");
+    }
+
+    @Test(priority = 1)
+    public void test1(){
+        System.out.println("Test 1 is running...");
+    }
+
+    @Test (priority = 2)
+    public void test2(){
+        System.out.println("Test 2 is running...");
+    }
+
+
+
+
+
+
+}
