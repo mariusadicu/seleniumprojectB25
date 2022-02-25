2fd40ebcc0c2765ef47c2651c4b6ba48b19a6484
MariusAD <mariusadicu@gmail.com>
2022-02-25 14:23:51 -0600

    .properties added

---
+++ src/test/java/com/cydeo/tests/day8_properties_config_reader/ReadingProperties.java
@@ -0,0 +1,16 @@
+package com.cydeo.tests.day8_properties_config_reader;
+
+import org.testng.annotations.Test;
+
+public class ReadingProperties {
+
+    @Test
+    public void reading_from_properties_test(){
+
+    }
+
+
+
+
+
+}
