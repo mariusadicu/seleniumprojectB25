11a043febd0814bc7b330ad8ee5636d77e04073c
MariusAD <mariusadicu@gmail.com>
2022-03-15 18:38:11 -0500

    end day13

--- src/test/java/com/cydeo/utilities/Driver.java
+++ src/test/java/com/cydeo/utilities/Driver.java
@@ -10,16 +10,16 @@ import java.util.concurrent.TimeUnit;
 public class Driver {
 
     /*
-   Creating a private constructor, so we are closing
-   access to the object of this class from outside the class
-    */
+       Creating a private constructor, so we are closing
+       access to the object of this class from outside the class
+        */
     private Driver(){}
 
     /*
     We make WebDriver private, because we want to close access from outside the class.
     We make it static because we will use it in a static method.
      */
-    private static WebDriver driver;
+    private static WebDriver driver; // value is null by default
 
     /*
     Create a re-usable utility method which will return same driver instance when we call it
@@ -34,10 +34,11 @@ public class Driver {
              */
             String browserType = ConfigurationReader.getProperty("browser");
 
+
             /*
-            Depending on the browserType that will be return from configuration.properties file
-            switch statement will determine the case, and open the matching browser
-             */
+                Depending on the browserType that will be return from configuration.properties file
+                switch statement will determine the case, and open the matching browser
+            */
             switch (browserType){
                 case "chrome":
                     WebDriverManager.chromedriver().setup();
@@ -51,12 +52,22 @@ public class Driver {
                     driver.manage().window().maximize();
                     driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
                     break;
-            }
 
+            }
         }
 
         return driver;
 
+    }
+
+    /*
+    This method will make sure our driver value is always null after using quit() method
+     */
+    public static void closeDriver(){
+        if (driver != null){
+            driver.quit(); // this line will terminate the existing session. value will not even be null
+            driver = null;
+        }
     }
 
 
